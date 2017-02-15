2f7de84c325b9577a4519c68c04d465cc8a055bd
Shadman Quazi <shadmanhquazi@gmail.com>
2017-02-15 13:24:08 -0500

    This is the landing page!

--- sketch.js
+++ sketch.js
@@ -1,33 +1,13 @@
-var song;
-var fft;
-
-function preload(){
-song = loadSound("derezzed.mp3");
-}
-
 function setup() {
-createCanvas(windowWidth,windowHeight);
-fft = new p5.FFT();
-song.play();
-strokeWeight(2);
+	createCanvas(windowWidth,windowHeight);
+	textSize(200);
+	textAlign(CENTER,CENTER);
+	background(255);
+	
+
+
 }
 
 function draw() {
-	var spectrum = fft.analyze();
-	var wave = fft.waveform();
-	//var AnaRed = map(spectrum[0],0,512,0,255);
-	//var AnaGreen = map(spectrum[256],0,512,0,255);
-	//var AnaBlue = map(spectrum[512],0,512,0,255);
-
-	background(255);
-
-	stroke(spectrum[32],spectrum[64],spectrum[128]);
-	
-	for (var i = 0; i < spectrum.length; i++){
-		var amp = spectrum[i];
-		var y = map(amp, 0,1024,height,-height*2);
-		line(i*2, height, i*2, y);
-	}
-	console.log(wave);
-  
+  text("Hi :)",width/2,height/2);
 }
