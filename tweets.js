a978293f76b9eac9cc5ff313563ca1d62a72e25b
Shadman Quazi <shadmanhquazi@gmail.com>
2017-02-18 18:04:20 -0500

    added some stuff to the string arrays

--- tweets.js
+++ tweets.js
@@ -1,17 +1,16 @@
 var prewords = ["ridiculous","idiotic","pointless","terrible"];
 var words = ["rumors","accusations","claims","ideas","thoughts"];
-var accusations = ["my gross incompetence", "my love affair with Putin","my tiny hands spilling \ncoffee on the nuclear codes","my failing marriage","my totally NOT Islamophobic ban","my VERY GOOD WALL"];
-var people = ["Bad Hombres","The Lizard People", "MSNBC", "Crooked Hillary and her team", "The illegals", "my sworn enemy, \n my conscience"];
-
-var exclamation = ["SAD!","LOSERS!","STUPID.","LIES."];
+var accusations = ["my gross incompetence", "my love affair with Putin","my tiny hands spilling coffee \non the nuclear codes","my failing marriage","my totally NOT Islamophobic ban","my VERY GOOD WALL"];
+var people = ["Bad Hombres","The Lizard People", "MSNBC", "Crooked Hillary and her team", "The illegals", "my sworn enemy, my conscience"];
+var altnames = ["fake news", "..well, those might actually be valid \npoints","a load of baloney","PURE LIES", "INSIDIOUS DISSENT", "SEVERE ACTS OF TREASON"];
+var exclamation = ["SAD!","LOSERS!","STUPID.","LIES.", "FIRED.","IDIOTS!","PUNISHMENT WILL BE HARSH!"];
 
 var font;
 //var img;
-var d = day();
 
 function preload()
 {
-	//font = loadFont("HelveticaNeue.ttf");
+	font = loadFont("HelveticaNeue Light.ttf");
 //	img = LoadImage("head.jpg");
 }
 
@@ -21,17 +20,16 @@ function setup() {
 	randoWord = int(random(words.length));
 	randoAccu = int(random(accusations.length));
 	randoPeeps = int(random(people.length));
+	randoAlt = int(random(altnames.length));
 	randoExcl = int(random(exclamation.length));
 
-	textFont("Comic Sans MS");
+	textFont(font);
 	textSize(30);
 	textAlign(LEFT)
 	text("@RealDonaldTrump",0,(height/2)-50);
 	textSize(60);
 	textAlign(LEFT, BASELINE);
-	text("The " + prewords[randoPreWord]+ " "  + words[randoWord] + " of " + accusations[randoAccu] + "\nare clearly fake news from " + people[randoPeeps] + ". " + exclamation[randoExcl],0,height/2);
-	text("Current day: \n" + d, 5, 50);
-
+	text("The " + prewords[randoPreWord]+ " "+ words[randoWord] +" of "+ accusations[randoAccu] +"\nare clearly " + altnames[randoAlt] + " from " + people[randoPeeps] + ".\n" + exclamation[randoExcl],0,height/2);
 }
 
 function draw() {
