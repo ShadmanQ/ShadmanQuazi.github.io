ec6620596f7fd4b5d82f6096054fbc00a8b84aa5
Shadman Quazi <shadmanhquazi@gmail.com>
2017-02-19 16:14:26 -0500

    added a timestamp generator
    Did some other stuff with fonts, and icons, just to make it look a bit more "Tweety"

--- tweets.js
+++ tweets.js
@@ -6,12 +6,14 @@ var altnames = ["fake news", "..well, those might actually be valid \npoints","a
 var exclamation = ["SAD!","LOSERS!","STUPID.","LIES.", "FIRED.","IDIOTS!","PUNISHMENT WILL BE HARSH!"];
 
 var font;
-//var img;
+var img;
 
 function preload()
 {
 	font = loadFont("HelveticaNeue Light.ttf");
-//	img = LoadImage("head.jpg");
+	font2 = loadFont("Helvetica Neu Bold.ttf");
+	img = loadImage("verified.jpg");
+	img2 = loadImage("head.jpg");
 }
 
 function setup() {
@@ -23,13 +25,26 @@ function setup() {
 	randoAlt = int(random(altnames.length));
 	randoExcl = int(random(exclamation.length));
 
+	image(img2,0,height/5,100,100);
+
+	textSize(40);
+	textFont(font2);
+	text("Donald J. Trump",100,(height/5)+40);
+
+	image(img,425,(height/5),100,100);
+
 	textFont(font);
-	textSize(30);
-	textAlign(LEFT)
-	text("@RealDonaldTrump",0,(height/2)-50);
+	text("@RealDonaldTrump",100,(height/5)+90);
 	textSize(60);
-	textAlign(LEFT, BASELINE);
-	text("The " + prewords[randoPreWord]+ " "+ words[randoWord] +" of "+ accusations[randoAccu] +"\nare clearly " + altnames[randoAlt] + " from " + people[randoPeeps] + ".\n" + exclamation[randoExcl],0,height/2);
+	textAlign(LEFT,TOP);
+	var tweet ="The " + prewords[randoPreWord]+ " "+ words[randoWord] +" of "+ accusations[randoAccu] +"\nare clearly " + altnames[randoAlt] + " from " + people[randoPeeps] + ".\n" + exclamation[randoExcl];
+	text(tweet,0,(height/5)+100);
+	tweetbox = font.textBounds(tweet,0,(height/5+100));
+
+	var timestamps = ["AM","PM"]
+	var months= ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"];
+	textSize(25);
+	text(+int(random(1,12)) +":" +int(random(9,59)) + random(timestamps) + " " + int(random(1,31)) + " " + random(months) + " 2017", tweetbox.x,tweetbox.y+tweetbox.h+220);
 }
 
 function draw() {
