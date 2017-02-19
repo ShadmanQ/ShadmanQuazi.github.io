ed13374171d0c01cf3a4efe6ae938c1772c80a27
Shadman Quazi <shadmanhquazi@gmail.com>
2017-02-19 16:47:30 -0500

    Added comments

--- tweets.js
+++ tweets.js
@@ -1,3 +1,4 @@
+//series of arrays for the tweet itself
 var prewords = ["ridiculous","idiotic","pointless","terrible"];
 var words = ["rumors","accusations","claims","ideas","thoughts"];
 var accusations = ["my gross incompetence", "my love affair with Putin","my tiny hands spilling coffee \non the nuclear codes","my failing marriage","my totally NOT Islamophobic ban","my VERY GOOD WALL"];
@@ -5,19 +6,26 @@ var people = ["Bad Hombres","The Lizard People", "MSNBC", "Crooked Hillary and h
 var altnames = ["fake news", "..well, those might actually be valid \npoints","a load of baloney","PURE LIES", "INSIDIOUS DISSENT", "SEVERE ACTS OF TREASON"];
 var exclamation = ["SAD!","LOSERS!","STUPID.","LIES.", "FIRED.","IDIOTS!","PUNISHMENT WILL BE HARSH!"];
 
+//variables to store asset files
 var font;
+var font2;
 var img;
+var img2;
 
 function preload()
 {
+	//Twitter uses a very specific font and I wanted to keep authenticity
 	font = loadFont("HelveticaNeue Light.ttf");
 	font2 = loadFont("Helvetica Neu Bold.ttf");
-	img = loadImage("verified.jpg");
+	//for the icon and verified symbol
+	img = loadImage("verified.png");
 	img2 = loadImage("head.jpg");
 }
 
 function setup() {
 	createCanvas(windowWidth,windowHeight);
+
+	//uses the random function to pick out one entry from each of the arrays
 	randoPreWord = int(random(prewords.length));
 	randoWord = int(random(words.length));
 	randoAccu = int(random(accusations.length));
@@ -25,26 +33,34 @@ function setup() {
 	randoAlt = int(random(altnames.length));
 	randoExcl = int(random(exclamation.length));
 
+	//places twitter icon on the page
 	image(img2,0,height/5,100,100);
 
-	textSize(40);
+	//places the username on the page
+	textSize(45);
 	textFont(font2);
 	text("Donald J. Trump",100,(height/5)+40);
 
-	image(img,425,(height/5),100,100);
+	//places the verified icon as well
+	image(img,450,(height/5),100,100);
 
+	//puts the twitter handle underneath the name
+	textSize(40)
 	textFont(font);
-	text("@RealDonaldTrump",100,(height/5)+90);
+	text("@RealDonaldTrump",100,(height/5)+100);
+	
+	//creates a fully formed tweet based on the phrases previously selected
 	textSize(60);
 	textAlign(LEFT,TOP);
 	var tweet ="The " + prewords[randoPreWord]+ " "+ words[randoWord] +" of "+ accusations[randoAccu] +"\nare clearly " + altnames[randoAlt] + " from " + people[randoPeeps] + ".\n" + exclamation[randoExcl];
 	text(tweet,0,(height/5)+100);
-	tweetbox = font.textBounds(tweet,0,(height/5+100));
+	tweetbox = font.textBounds(tweet,0,(height/5)+100,60);
 
+	//generates a random timestamp
 	var timestamps = ["AM","PM"]
 	var months= ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"];
 	textSize(25);
-	text(+int(random(1,12)) +":" +int(random(9,59)) + random(timestamps) + " " + int(random(1,31)) + " " + random(months) + " 2017", tweetbox.x,tweetbox.y+tweetbox.h+220);
+	text(+int(random(1,12)) +":" +int(random(9,59)) + random(timestamps) + "  -  " + int(random(1,31)) + " " + random(months) + " 2017", tweetbox.x,tweetbox.y+tweetbox.h+220);
 }
 
 function draw() {
